07cf24caf551ab8f3b8655e955c28f53fe2da833
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-08-10 01:30:33 +0600

    first commit

---
+++ 1_Mutable and Immutable Object/example.py
@@ -0,0 +1,13 @@
+
+# Mutable objects can be modified after they are created,
+# while immutable objects cannot be changed once they are created.
+
+
+
+x=10 
+y=x
+print("x:",x,"y:",y)
+x=15
+print("x:",x,"y:",y)
+
+ 
