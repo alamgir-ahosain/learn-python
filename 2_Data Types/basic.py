c2f577c4f15ae52335e54e10cee64ed8fbd72287
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-08-10 09:55:01 +0600

    declared and print , no opeation included

---
+++ 2_Data Types/basic.py
@@ -0,0 +1,38 @@
+
+# Integer : positive and negative
+x=10 ; print(x)
+
+# Float : decimal number
+pi=3.14 ; print(pi)
+
+# Complex : real and imaginary part
+z=2+3j ; print(z)
+
+# Boolean : True or False Value
+check=True ; print(check)
+
+# String: text ,immutable object
+name="Alamgir" ; print(name)
+#name[0]='B'  #TypeError: 'name' object does not support item assignment
+
+# List : ordered,mutable
+fruits=['Mango','Banana','Jack Fruit'] ; print(fruits)
+
+# Touple : ordered , immutable
+colors=('Red','Green','Blue') ; print(colors)
+
+# Set : unique,unordered,mutable
+st={1,2,3,1} ; print(st) # output : 1,2,3
+
+# Dictionary : key value pairs,mutable
+dt={'name':'Alamgir','id':'CE21012','cgpa':3.5} ; print(dt)
+
+# None Type
+val=None ; print(val) # output :None
+
+# Bytes : immutable binary data
+b = b'hello'
+print(type(b))  # Output: <class 'bytes'>
+
+# Range : sequence  ,used in loop
+r=range(5) ; print(r)   # output : range(0,5)
