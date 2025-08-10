8a06506439fbafad531cbc609c3097738659168c
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-08-11 01:21:25 +0600

    problem solution one by  one

---
+++ 5_Loop/2.py
@@ -0,0 +1,10 @@
+"""
+    2. Sum of Even Numbers
+    Problem: Calculate the sum of even numbers up to a given number n.
+"""
+
+n=int(input("Enter a number:"))
+sum=0
+for i in range(0,n+1,2):
+        sum+=i
+print(sum)        
